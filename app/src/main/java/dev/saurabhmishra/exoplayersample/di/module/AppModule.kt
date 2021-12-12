c111a5455a8559ed3f1f085b37e62099883f9eff
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-12-12 17:22:39 +0530

    add: exo player to the di graph

--- app/src/main/java/dev/saurabhmishra/exoplayersample/di/module/AppModule.kt
+++ app/src/main/java/dev/saurabhmishra/exoplayersample/di/module/AppModule.kt
@@ -1,13 +1,23 @@
 package dev.saurabhmishra.exoplayersample.di.module
 
+import android.content.Context
+import com.google.android.exoplayer2.ExoPlayer
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
 
-
+    @Provides
+    @Singleton
+    fun provideExoplayer(context: Context): ExoPlayer {
+        return ExoPlayer.Builder(context).build()
+    }
 
 }
