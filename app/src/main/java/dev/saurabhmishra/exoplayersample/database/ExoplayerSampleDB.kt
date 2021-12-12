08a5103d3555b905ff59bbfeecd51b21215b50a3
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-12-12 17:11:51 +0530

    add: dao methods added to database class

--- app/src/main/java/dev/saurabhmishra/exoplayersample/database/ExoplayerSampleDB.kt
+++ app/src/main/java/dev/saurabhmishra/exoplayersample/database/ExoplayerSampleDB.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import dev.saurabhmishra.exoplayersample.database.dao.CommentDao
+import dev.saurabhmishra.exoplayersample.database.dao.UserDao
+import dev.saurabhmishra.exoplayersample.database.dao.VideoDao
 import dev.saurabhmishra.exoplayersample.database.entity.CommentEntity
 import dev.saurabhmishra.exoplayersample.database.entity.UserEntity
 import dev.saurabhmishra.exoplayersample.database.entity.VideoEntity
@@ -17,6 +20,11 @@ import dev.saurabhmishra.exoplayersample.utils.AppConstants
     ], version = AppConstants.Database.VERSION
 )
 abstract class ExoplayerSampleDB : RoomDatabase() {
+
+    abstract fun videoDao(): VideoDao
+    abstract fun userDao(): UserDao
+    abstract fun commentDao(): CommentDao
+
     companion object {
         fun create(context: Context): ExoplayerSampleDB {
             return Room.databaseBuilder(
