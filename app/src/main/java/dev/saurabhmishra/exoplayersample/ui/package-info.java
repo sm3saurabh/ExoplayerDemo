2f4b88687174a682243169ed1de270f5681961cc
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-12-12 10:41:48 +0530

    add: epoxy to the project

---
+++ app/src/main/java/dev/saurabhmishra/exoplayersample/ui/package-info.java
@@ -0,0 +1,10 @@
+package dev.saurabhmishra.exoplayersample.ui;
+
+import com.airbnb.epoxy.EpoxyDataBindingPattern;
+
+import dev.saurabhmishra.exoplayersample.R;
+
+@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "epoxy_layout")
+interface EpoxyDataBindingConfig {
+
+}
