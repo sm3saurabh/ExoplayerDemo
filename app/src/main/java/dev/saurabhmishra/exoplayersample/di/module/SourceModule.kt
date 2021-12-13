2e5fbf76530a423c99eb3328e701ea2643aa4120
Saurabh Mishra <sm3.saurabhmishra@gmail.com>
2021-12-13 10:02:05 +0530

    add: source module provided

---
+++ app/src/main/java/dev/saurabhmishra/exoplayersample/di/module/SourceModule.kt
@@ -0,0 +1,46 @@
+package dev.saurabhmishra.exoplayersample.di.module
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dev.saurabhmishra.data.sources.CommentsLocalSource
+import dev.saurabhmishra.data.sources.CommentsRemoteSource
+import dev.saurabhmishra.data.sources.VideoLocalSource
+import dev.saurabhmishra.data.sources.VideoRemoteSource
+import dev.saurabhmishra.exoplayersample.database.ExoplayerSampleDB
+import dev.saurabhmishra.exoplayersample.network.Api
+import dev.saurabhmishra.exoplayersample.source.CommentsLocalSourceImpl
+import dev.saurabhmishra.exoplayersample.source.CommentsRemoteSourceImpl
+import dev.saurabhmishra.exoplayersample.source.VideoLocalSourceImpl
+import dev.saurabhmishra.exoplayersample.source.VideoRemoteSourceImpl
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object SourceModule {
+
+    @Provides
+    @Singleton
+    fun provideCommentsLocalSource(db: ExoplayerSampleDB): CommentsLocalSource {
+        return CommentsLocalSourceImpl(db)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCommentsRemoteSource(api: Api): CommentsRemoteSource {
+        return CommentsRemoteSourceImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideVideoRemoteSource(api: Api): VideoRemoteSource {
+        return VideoRemoteSourceImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideVideoLocalSource(db: ExoplayerSampleDB): VideoLocalSource {
+        return VideoLocalSourceImpl(db)
+    }
+}
